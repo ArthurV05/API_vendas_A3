dea73446036a81cb7a47befcffacf21d21801976
pedroarthurvalente-design <pedroarthurvalente@gmail.com>
2026-06-07 12:05:31 -0300

    fix: reorganiza pasta models e remodela as classes

---
+++ models/produto.js
@@ -0,0 +1,8 @@
+class Produto {
+    constructor(id, nome, preco, quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
