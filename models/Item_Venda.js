64173e4ae10ab3b1209b446dd9bbd05caeeb1fef
pedroarthurvalente-design <pedroarthurvalente@gmail.com>
2026-06-15 13:51:35 -0300

    feat: Adiciona as classes models, que contém entidades do mundo real representadas no código

--- models/Item_Venda.js
+++ models/Item_Venda.js
@@ -1,10 +1,10 @@
 class Item_Venda {
-    constructor(id, venda, produto, quantidade, valorUnitario) {
+    constructor(id, venda, produto, quantidade, preco_unitario) {
         this.id = id;
         this.venda = venda;
         this.produto = produto;
         this.quantidade = quantidade;
-        this.valorUnitario = valorUnitario;
+        this.preco_unitario = preco_unitario;
     }
 }
 
